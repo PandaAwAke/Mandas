c442af64c132f258296031ec056990e3f84578e0
Ma Yingshuo <191220080@smail.nju.edu.cn>
2020-12-06 20:20:27 +0800

    Lecture 16: Bug(mousebutton) fixed, imgui events added.

--- Mandas/src/Mandas/ImGui/ImGuiLayer.h
+++ Mandas/src/Mandas/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Mandas/Layer.h"
 
+#include "Mandas/Events/ApplicationEvent.h"
+#include "Mandas/Events/KeyEvent.h"
+#include "Mandas/Events/MouseEvent.h"
+
 namespace Mandas {
 	class MANDAS_API ImGuiLayer : public Layer
 	{
@@ -13,7 +17,17 @@ namespace Mandas {
 		void OnDetach() override;
 		void OnUpdate() override;
 		void OnEvent(Event& event) override;
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 
+		
 	private:
 		float m_Time = 0.0;
 
