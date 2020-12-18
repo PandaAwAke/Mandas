406721a372e84e2a4aed1b2e33b49a5f1658df2b
Ma Yingshuo <191220080@smail.nju.edu.cn>
2020-12-19 01:02:16 +0800

    Lecture 38: Transforms.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -3,6 +3,8 @@
 
 #include "ImGui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Mandas::Layer
 {
 public:
@@ -35,10 +37,10 @@ public:
 
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f,  0.0f,
-			 0.75f, -0.75f,  0.0f,
-			 0.75f,  0.75f,  0.0f,
-			-0.75f,  0.75f,  0.0f
+			-0.5f, -0.5f,  0.0f,
+			 0.5f, -0.5f,  0.0f,
+			 0.5f,  0.5f,  0.0f,
+			-0.5f,  0.5f,  0.0f
 		};
 		m_SquareVA.reset(Mandas::VertexArray::Create());
 		std::shared_ptr<Mandas::VertexBuffer> squareVB;
@@ -61,6 +63,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 			
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 			
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -69,7 +72,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -95,13 +98,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 			
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 			
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -147,8 +151,20 @@ public:
 		m_Camera.SetRotation(m_CameraRotation);
 
 		Mandas::Renderer::BeginScene(m_Camera);
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 		
-		Mandas::Renderer::Submit(m_BlueShader, m_SquareVA);
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				//glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_SquarePosition);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Mandas::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+		
 		Mandas::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Mandas::Renderer::EndScene();
@@ -180,7 +196,6 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 50.0f;
-
 };
 
 class Sandbox : public Mandas::Application
